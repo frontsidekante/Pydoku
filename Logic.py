25c485344484f776a74fbb2c13bfad7a300f7dd2
Vicky <s52963@beuth-hochschule.de>
2016-01-07 20:35:59 +0100

    Initial commit

---
+++ Logic.py
@@ -0,0 +1,19 @@
+import random
+
+numbers = [1,2,3,4,5,6,7,8,9]
+
+print type(numbers)
+
+for i in numbers:
+    print i
+
+print type(numbers)
+
+# Shuffle returns none
+random.shuffle(numbers)
+
+print numbers
+row1 = numbers
+
+# row2 shuffle
+
