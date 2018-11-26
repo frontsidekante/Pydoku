f71c15804b0a3a4455311cc40cd2b936aa72f230
Vicky <v.koehring@gmail.com>
2018-11-26 09:44:46 +0100

    update

---
+++ logic.py
@@ -0,0 +1,12 @@
+# import copy
+import random
+# import os
+
+
+# Create random starter numbers
+import pdb; pdb.set_trace()
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+random.shuffle(numbers)
+print(numbers)
+
+
